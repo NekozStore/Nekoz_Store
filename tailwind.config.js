95850a87a2ff044e20bfc96012989e7415d7340e
NekozStore <whiteecatz01@gmail.com>
2024-05-12 13:59:20 +0530

    Added Products and folder changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-pink': '#F7B2BD', // Adjusted to a lighter shade for a softer appearance
+        'brand-blue': '#B1DDF1', // A gentle blue, complementing the pink
+      },
+      fontFamily: {
+        'heading': ['Pacifico', 'cursive'], // For headings to showcase friendliness
+        'body': ['Montserrat', 'sans-serif'], // For body text, ensuring legibility
+        'sub-heading': ['Amatic SC', 'cursive'], // Added based on your design for sub-headings
+      },
+    },
   },
   plugins: [],
 }
